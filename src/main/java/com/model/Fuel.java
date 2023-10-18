301caff23fe8a6ba02810f545bfc09d3e966bcf0
navindunimsara2001 <141642402+navindunimsara2001@users.noreply.github.com>
2023-10-18 11:20:58 +0530

    class path changes

--- src/main/java/com/model/Fuel.java
+++ src/main/java/com/model/Fuel.java
@@ -1,5 +1,21 @@
 package com.model;
 
 public class Fuel {
-
+	private String type;
+	private float amount;
+	
+	public String getType() {
+		return type;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	
+	
 }
