22c5bb80ff2d33f20e4502c3070e8331ddeccbcd
yehan2002 <yehanjaya2002@gmail.com>
2023-10-18 11:04:43 +0530

    Update servlet.

--- src/main/java/com/servlet/HelloServlet.java
+++ src/main/java/com/servlet/HelloServlet.java
@@ -1,5 +1,9 @@
 package com.servlet;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
