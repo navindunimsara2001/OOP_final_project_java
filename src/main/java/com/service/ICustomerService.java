802b94e1a91024994addfac22ab1b54cd3b90464
navindunimsara2001 <141642402+navindunimsara2001@users.noreply.github.com>
2023-10-18 13:13:41 +0530

    services Interfaces

--- src/main/java/com/service/ICustomerService.java
+++ src/main/java/com/service/ICustomerService.java
@@ -1,6 +1,38 @@
 package com.service;
 
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import com.model.Customer;
+
 public interface ICustomerService {
-	// initialize logger 
-	public static final logger = 
+	// initialize logger
+	public static final Logger logger = Logger.getLogger(ICustomerService.class.getName());
+	
+	/**
+	 *  add customer to data base
+	 * @param cus
+	 */
+	public void addCustomer(Customer cus);
+	
+	/**
+	 *  get customer details by using id
+	 * @param ID
+	 * @return
+	 */
+	public Customer getCustomerById(int ID);
+	
+	/**
+	 * get all customers in database
+	 * @return
+	 */
+	public ArrayList<Customer> getAllCustomers();
+	
+	/**
+	 * update customer details
+	 * @param ID
+	 * @param cus
+	 */
+	public void updateCustomer(int ID , Customer cus);
+	
 }
