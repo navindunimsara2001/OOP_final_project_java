08671d5db718742ee4616803b51833db3ce6d423
yehan2002 <yehanjaya2002@gmail.com>
2023-10-18 12:32:59 +0530

    Add DBUtil.

---
+++ src/main/java/com/util/DBUtil.java
@@ -0,0 +1,26 @@
+package com.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+
+    private static final String DBUrl = "jdbc:mysql://localhost/winFuel";
+    private static final String user = "OOPacc";
+    private final static String password = "1234";
+
+    public static Connection connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            return DriverManager.getConnection(DBUrl, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
