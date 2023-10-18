6ea64e59c1aed7a68d7fa6245774f61167b3443d
yehan2002 <yehanjaya2002@gmail.com>
2023-10-18 13:25:08 +0530

    Fix formatting.

--- src/main/java/com/model/Fuel.java
+++ src/main/java/com/model/Fuel.java
@@ -1,21 +1,24 @@
 package com.model;
 
 public class Fuel {
-	private String type;
-	private float amount;
-	
-	public String getType() {
-		return type;
-	}
-	public float getAmount() {
-		return amount;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public void setAmount(float amount) {
-		this.amount = amount;
-	}
-	
-	
+    private String type;
+    private float amount;
+
+    public String getType() {
+        return type;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+
 }
