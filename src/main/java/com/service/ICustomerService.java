c1c3bd0e8881ef7266d621b141bad6944567dc48
yehan2002 <yehanjaya2002@gmail.com>
2023-10-18 13:16:36 +0530

    Fix doc comments.

--- src/main/java/com/service/ICustomerService.java
+++ src/main/java/com/service/ICustomerService.java
@@ -6,33 +6,37 @@ import java.util.logging.Logger;
 import com.model.Customer;
 
 public interface ICustomerService {
-	// initialize logger
-	public static final Logger logger = Logger.getLogger(ICustomerService.class.getName());
-	
-	/**
-	 *  add customer to data base
-	 * @param cus
-	 */
-	public void addCustomer(Customer cus);
-	
-	/**
-	 *  get customer details by using id
-	 * @param ID
-	 * @return
-	 */
-	public Customer getCustomerById(int ID);
-	
-	/**
-	 * get all customers in database
-	 * @return
-	 */
-	public ArrayList<Customer> getAllCustomers();
-	
-	/**
-	 * update customer details
-	 * @param ID
-	 * @param cus
-	 */
-	public void updateCustomer(int ID , Customer cus);
-	
+    // initialize logger
+    public static final Logger logger = Logger.getLogger(ICustomerService.class.getName());
+
+    /**
+     * add customer to database
+     *
+     * @param cus the customer
+     */
+    public void addCustomer(Customer cus);
+
+    /**
+     * get customer details by using id
+     *
+     * @param ID the id of the customer
+     * @return the customer
+     */
+    public Customer getCustomerById(int ID);
+
+    /**
+     * get all customers in database
+     *
+     * @return a list of all customers
+     */
+    public ArrayList<Customer> getAllCustomers();
+
+    /**
+     * update customer details
+     *
+     * @param ID  the id of the customer
+     * @param cus the new details of the customer.
+     */
+    public void updateCustomer(int ID, Customer cus);
+
 }
