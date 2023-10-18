3f55b2a564fefe251bfc9a4882d5bacdd9b86d64
navindunimsara2001 <141642402+navindunimsara2001@users.noreply.github.com>
2023-10-18 13:25:53 +0530

    enhancements

---
+++ src/main/java/com/model/Item.java
@@ -0,0 +1,21 @@
+package com.model;
+
+public class Item {
+	//Attributes
+	private int ID;
+	private String name;
+	
+	// getters & setters
+	public int getID() {
+		return ID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
--- src/main/java/com/model/ItemRequest.java
+++ src/main/java/com/model/ItemRequest.java
@@ -1,5 +1,31 @@
 package com.model;
 
 public class ItemRequest {
-
+	
+	// attributes
+	private int ID;
+	private Item itemId;
+	private int qty;
+	
+	// getters & setters
+	public int getID() {
+		return ID;
+	}
+	public Item getItemId() {
+		return itemId;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public void setItemId(Item itemId) {
+		this.itemId = itemId;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	
+	
 }
