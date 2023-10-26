61113a7a5110ec532d5b939bf5c307fa36dc3148
yehan2002 <yehanjaya2002@gmail.com>
2023-10-26 15:33:29 +0530

    Load db config from properties file.

--- src/main/java/com/util/DBUtil.java
+++ src/main/java/com/util/DBUtil.java
@@ -1,14 +1,36 @@
 package com.util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Objects;
+import java.util.Properties;
 
 public class DBUtil {
 
-    private static final String DBUrl = "jdbc:mysql://localhost/winFuel";
-    private static final String user = "OOPacc";
-    private final static String password = "1234";
+    private static final String DBUrl;
+    private static final String user;
+    private final static String password;
+
+    static {
+        InputStream propInput = DBUtil.class.getClassLoader().getResourceAsStream("db.properties");
+        Properties properties = new Properties();
+
+        try {
+            properties.load(propInput);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load db details from");
+        }
+
+        String host = Objects.requireNonNull(properties.getProperty("host"));
+        String db = Objects.requireNonNull(properties.getProperty("dbName"));
+        user = Objects.requireNonNull(properties.getProperty("username"));
+        password = Objects.requireNonNull(properties.getProperty("password"));
+        DBUrl = "jdbc:mysql://" + host + "/" + db;
+    }
+
 
     /**
      * loads the mysql jdbc driver and creates a new connection to the database
@@ -16,6 +38,8 @@ public class DBUtil {
      * @return a new connection
      */
     public static Connection connect() {
+
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
